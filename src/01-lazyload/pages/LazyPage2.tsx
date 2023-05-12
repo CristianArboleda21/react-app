3b6f6fb40b8a4d21d86c6ead7f140dad2e627045
Cristian <cristian.arboleda02@gmail.com>
2023-05-12 11:42:05 -0500

    rutas lazyload

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,10 @@
+
+
+export const LazyPage2 = () => {
+  return (
+    <div>LazyPage 2</div>
+  )
+}
+
+export default LazyPage2;
+
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,40 +1,42 @@
 import { BrowserRouter } from "react-router-dom"
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom'
 import logo from '../logo.svg'
+import { routes } from "./routes"
+import { Suspense } from "react"
 
 export const Navigation = () => {
   return (
     <>
-        <BrowserRouter>
-            <div className="main-layout">
-                <nav>
-                    <img src={ logo } alt="Recat Logo" />
+        <Suspense fallback={ <span>Loading...</span>}>
+            <BrowserRouter>
+                <div className="main-layout">
+                    <nav>
+                        <img src={ logo } alt="Recat Logo" />
 
-                    <ul>
-                        <li>
-                            <NavLink to='/home'>Home</NavLink>
-                        </li>
+                        <ul>
+                            {
+                                routes.map( ({ to, name}) => (
+                                    <li key={ to }>  
+                                        <NavLink to={ to }> { name} </NavLink>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </nav>
 
-                        <li>
-                            <NavLink to='/about'>About</NavLink>
-                        </li>
+                    <Routes>
+                        {
+                            routes.map( ({ path, Component }) => (
+                                <Route key={ path } path={ path } element={ <Component/> } />
+                            ))
+                        }
 
-                        <li>
-                            <NavLink to='/users'>User</NavLink>
-                        </li>
-                    </ul>
-                </nav>
+                        <Route path="/*" element={ <Navigate to={routes[0].to} replace /> } />
 
-                <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> } />
-                    <Route path="users" element={ <h1>Users Page</h1> } />
-                    <Route path="home" element={ <h1>Home Page</h1> } />
-
-                    <Route path="/*" element={ <Navigate to='/home' replace /> } />
-
-                </Routes>
-            </div>
-        </BrowserRouter>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </Suspense>
     </>
   )
 }
